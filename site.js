3be55cad1a5e3915ea2386c7c5df3547120d97ac
tatsumadesign <tatsumadesign@gmail.com>
2023-11-08 21:41:47 -0600

    Progress
    Progress
    But JS isn't working. Maybe need to look into html list.

--- site.js
+++ site.js
@@ -1,2 +1,12 @@
-console.log('Hello world');	
+const taskInput = document.getElementById('toDoItem');
+const addBtn = document.getElementById('enter');
+const taskList = document.getElementById('taskList');
+
+addBtn.addEventListener("click", () => {
+    const taskText = taskInput.value.trim();
+    if (taskText !== "") {
+        createTask(taskText);
+        taskInput.value = "";
+    }
+});
 
